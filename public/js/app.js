838d9af3c07f3a83f936b0b2d4dc03f4b3d11881
Date, Shivanand [VPE] <shivanand.date@medtronic.com>
2021-02-22 14:37:12 +0530

    Remove unnecessary log calls

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Loaded clientside js file!");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
